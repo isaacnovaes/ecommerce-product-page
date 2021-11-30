d96d22a17fc509904cc8a2702d68c470d4912c17
Isaac Novaes <isaac.novaes@hotmail.com>
2021-11-30 06:22:21 +0100

    Update file. Fix the bug where the the slide functions are called twice

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const bgBlurMobile = document.querySelector(".bgBlur-mobile");
 const bgBlurDesktop = document.querySelector(".bgBlur-desktop");
 const close = document.getElementById("close");
 
-const mainSlider = document.querySelector("main .slider");
+const mainSlider = document.querySelector(".slider");
 const mainSlides = document.querySelectorAll(".slide");
-const slideButtonLeft = document.querySelector(".slider .button-left");
-const slideButtonRight = document.querySelector(".slider .button-right");
-const slideThumbnailContainer = document.querySelector(".thumbnail-container");
+const mainButtonLeft = document.querySelector(".slider .button-left");
+const mainButtonRight = document.querySelector(".slider .button-right");
+const mainThumbnailContainer = document.querySelector(".thumbnail-container");
 
 const lightboxSlides = document.querySelectorAll(".lightbox-slide");
 const lightboxButtonLeft = document.querySelector(".lightbox-button-left");
@@ -18,12 +18,14 @@ const lightboxButtonRight = document.querySelector(".lightbox-button-right");
 const lightboxThumbnailContainer = document.querySelector(
 	".lightbox-thumbnail-container"
 );
-const closeLightbox = document.querySelector(".button-close-lightbox");
 
+const closeLightbox = document.querySelector(".button-close-lightbox");
 const lightboxModal = document.querySelector(".lightbox-modal");
 
 let currentSlide = 0;
 
+/////////////////////////////////////////////////
+// handles hamburger toggle
 const toggleMenuNav = () => {
 	navMenu.classList.toggle("nav-menu-active");
 	bgBlurMobile.classList.toggle("show-bg");
@@ -32,9 +34,22 @@ const toggleMenuNav = () => {
 hamburger.addEventListener("click", () => {
 	toggleMenuNav();
 });
+
 close.addEventListener("click", () => {
 	toggleMenuNav();
 });
+
+/////////////////////////////////////////////////
+// define functions used by the sliders
+// slider -> slides parent element, which is the container for the slides
+// slides -> elements that should move
+// there are two slides: mainSlides and lightboxSlides
+const goToSlide = function (slides) {
+	slides.forEach(
+		(slide, index) =>
+			(slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`)
+	);
+};
 
 const showSelectedThumbnail = thumbnailContainer => {
 	thumbnailContainer
@@ -45,85 +60,109 @@ const showSelectedThumbnail = thumbnailContainer => {
 		.classList.add("thumbnail-selected");
 };
 
-showSelectedThumbnail(slideThumbnailContainer);
-
-// prettier-ignore
-const slider = function (allSlides, buttonLeft, buttonRight, thumbnailContainer) {
-
-	const maxSlide = allSlides.length;
-
-	const goToSlide = function (slideToShow) {
-		allSlides.forEach(
-			(slide, index) =>
-				(slide.style.transform = `translateX(${100 * (index - slideToShow)}%)`)
-		);
-	};	
-
-	const goToNextSlide = function () {
-		currentSlide === maxSlide - 1 ? (currentSlide = 0) : currentSlide++;
-		goToSlide(currentSlide);
-		showSelectedThumbnail(thumbnailContainer);
-	};
-
-	const goToPreviousSlide = function () {
-		currentSlide === 0 ? (currentSlide = maxSlide - 1) : currentSlide--;
-		goToSlide(currentSlide);
-		showSelectedThumbnail(thumbnailContainer);
-	};
-
-	const init = function () {
-		goToSlide(currentSlide);
-		showSelectedThumbnail(thumbnailContainer);
-	};
-
-	init();
-
-	// Event handlers
-	buttonRight.addEventListener("click", goToNextSlide);
-	buttonLeft.addEventListener("click", goToPreviousSlide);
-
-	document.addEventListener("keydown", function (event) {
-		if (event.key === "ArrowRight") {
-			goToNextSlide();
-			showSelectedThumbnail(thumbnailContainer);
-		}
-
-		if (event.key === "ArrowLeft") {
-			goToPreviousSlide();
-			showSelectedThumbnail(thumbnailContainer);
-		}
-	});
-
-	thumbnailContainer.addEventListener("click", event => {
-		if (!event.target.classList.contains("thumbnail")) return;
-
-		currentSlide = event.target.dataset.thumb; // currentSlide needs to be updated first
-		// so that showSelectedThumbnail() works properly
-
-		goToSlide(currentSlide);
-
-		showSelectedThumbnail(thumbnailContainer);
-	});
+const moveSlides = slides => {
+	goToSlide(slides);
+	showSelectedThumbnail(
+		slides[0].parentElement.parentElement.querySelector(".thumb-cont")
+	);
 };
 
-slider(mainSlides, slideButtonLeft, slideButtonRight, slideThumbnailContainer);
+const goToNextSlide = function (slides) {
+	currentSlide === slides.length - 1 ? (currentSlide = 0) : currentSlide++;
+	moveSlides(slides);
+};
 
-mainSlider.addEventListener("click", event => {
-	
-	bgBlurDesktop.classList.toggle("show-bg");
-	lightboxModal.classList.toggle("show-lightbox-modal");
+const goToPreviousSlide = function (slides) {
+	currentSlide === 0 ? (currentSlide = slides.length - 1) : currentSlide--;
+	moveSlides(slides);
+};
 
-	currentSlide = event.target.closest(".slide").dataset.slide;
+/////////////////////////////////////////////////
+// initiate main slider
+moveSlides(mainSlides);
 
-	slider(
-		lightboxSlides,
-		lightboxButtonLeft,
-		lightboxButtonRight,
-		lightboxThumbnailContainer
-	);
+/////////////////////////////////////////////////
+// Event handlers for buttons from main
+mainButtonRight.addEventListener("click", () => goToNextSlide(mainSlides));
+mainButtonLeft.addEventListener("click", () => goToPreviousSlide(mainSlides));
+
+mainThumbnailContainer.addEventListener("click", event => {
+	if (!event.target.classList.contains("thumbnail")) return;
+
+	currentSlide = +event.target.dataset.thumb;
+	// currentSlide needs to be updated first
+	// so that showSelectedThumbnail() works properly
+
+	moveSlides(mainSlides);
 });
 
-closeLightbox.addEventListener("click", () => {
+/////////////////////////////////////////////////
+// handles correct lightbox slide display after clicking on a slide from main
+mainSlider.addEventListener("click", event => {
+	// prevent lightbox to appear in mobile screen size
+	if (getComputedStyle(hamburger).display !== "none") return;
+
 	bgBlurDesktop.classList.toggle("show-bg");
 	lightboxModal.classList.toggle("show-lightbox-modal");
+
+	currentSlide = +event.target.closest(".slide").dataset.slide;
+	// update currentSlide to the slide clicked
+
+	moveSlides(lightboxSlides);
+
+	lightboxButtonRight.addEventListener("click", () =>
+		goToNextSlide(lightboxSlides)
+	);
+	lightboxButtonLeft.addEventListener("click", () =>
+		goToPreviousSlide(lightboxSlides)
+	);
+
+	lightboxThumbnailContainer.addEventListener("click", event => {
+		if (!event.target.classList.contains("thumbnail")) return;
+
+		currentSlide = +event.target.dataset.thumb;
+		// currentSlide needs to be updated first
+		// so that showSelectedThumbnail() works properly
+
+		moveSlides(lightboxSlides);
+	});
+});
+
+/////////////////////////////////////////////////
+// handles keyboard navigation
+const defineSlidesToWork = (slides, event) => {
+	if (event.key === "ArrowRight") goToNextSlide(slides);
+	if (event.key === "ArrowLeft") goToPreviousSlide(slides);
+};
+
+document.addEventListener("keydown", event => {
+	if (lightboxModal.classList.contains("show-lightbox-modal")) {
+		// if the lightbox modal is active, handles keyboard navigation on the lightbox modal
+		defineSlidesToWork(lightboxSlides, event);
+	} else {
+		// else (in this case the lightbox is inactive), handles keyboard navigation on the main slides
+		defineSlidesToWork(mainSlides, event);
+	}
+});
+
+/////////////////////////////////////////////////
+// handles lightbox modal closing 
+const toggleLightboxModal = () => {
+	bgBlurDesktop.classList.toggle("show-bg");
+	lightboxModal.classList.toggle("show-lightbox-modal");
+
+	// update main slides
+	moveSlides(mainSlides);
+};
+
+bgBlurDesktop.addEventListener("click", () => toggleLightboxModal());
+
+closeLightbox.addEventListener("click", () => {
+	toggleLightboxModal();
+});
+
+document.addEventListener("keydown", event => {
+	if (!lightboxModal.classList.contains("show-lightbox-modal")) return;
+
+	if (event.key === "Escape") toggleLightboxModal();
 });
